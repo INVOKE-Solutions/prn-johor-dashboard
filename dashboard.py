781d20a1f2127663f48a1779828d1b0d7095539e
fatinroslan96 <fatin.nabihah1996@gmail.com>
2022-03-01 15:58:38 +0800

    added config.toml and added the colour to represent winning party on choropleth map

--- dashboard.py
+++ dashboard.py
@@ -15,7 +15,8 @@ import pyautogui
 
 
 
-
+#to be commented
+#to hide the menu bar on the streamlit page
 # st.markdown(""" <style>
 # #MainMenu {visibility: hidden;}
 # footer {visibility: hidden;}
@@ -25,15 +26,11 @@ st.markdown("""
 <link href='http://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
 """, unsafe_allow_html=True)
 
-# st.markdown("""
-# <div class="block-container css-12oz5g7 eknhn3m2" style="position: relative;padding-top: 5%">
-# </div>
-# """, unsafe_allow_html=True)
-
+#the header with the line
 st.markdown("<h2 style='height: 2em;overflow:auto;color: #4682B4;font-size:25px;border-bottom: 1px solid #ccc;font-family: Lato, sans-serif;font-weight:900;padding-top: 0%'>PRN Johor Dashboard</h2>", unsafe_allow_html=True)
 
 
-#dun johor geojson
+#import the dun johor geojson file to plot the coordinates for dun border
 repo_url = 'https://raw.githubusercontent.com/TindakMalaysia/Johor-Maps/master/2017/PROPOSAL/DUN/Johor_Syor_2_DUN_2017.geojson'
 my_regions_geo_dun = requests.get(repo_url).json()
 
@@ -41,7 +38,6 @@ dun_dict = {}
 dun_par_dict = {}
 
 for i in range(len(my_regions_geo_dun['features'])):
-    # lst_dun.append(my_regions_geo_dun['features'][i]['properties']['DUN'])
     final_str = str(my_regions_geo_dun['features'][i]['properties']['KodDUN'])
 
     if my_regions_geo_dun['features'][i]['properties']['DUN'] not in dun_dict:
@@ -52,23 +48,7 @@ for i in range(len(my_regions_geo_dun['features'])):
 
 
 
-#parliament johor geojson
-repo_url = "https://raw.githubusercontent.com/TindakMalaysia/Johor-Maps/master/2017/PROPOSAL/PAR/Johor_Syor_2_PAR_2017.geojson"
-my_regions_geo_par = requests.get(repo_url).json()
-
-
-lst_par = []
-par_dict = {}
-
-for i in range(len(my_regions_geo_par['features'])):
-    lst_par.append(my_regions_geo_par['features'][i]['properties']['Parliament'])
-    final_str = str(my_regions_geo_par['features'][i]['properties']['KodPar'])
-
-    if my_regions_geo_par['features'][i]['properties']['Parliament'] not in par_dict:
-        par_dict[my_regions_geo_par['features'][i]['properties']['Parliament']] = final_str
-
-
-
+#import the results from the model
 df = pd.read_excel("Johor PRN.xlsx", sheet_name='transposed')
 df = df.rename({'PH': 'PH_1', 'Non-PH': 'Non-PH_1', 'Not Sure/Others': 'Not Sure/Others_1'}, axis=1)
 
@@ -78,6 +58,8 @@ df['Not Sure/Others_1'].replace(np.nan, 0, inplace=True)
 
 
 #recalculate the % to take into acount only the 3 columns
+#because in the original sample data the % include PH, non PH, fence sitter and refused to answer
+#the new model will only output % for PH and Non PH---------------------------------------------can remove from this point onwards
 def recalculate_nonph(x):
     non_ph = x['Non-PH_1']
     not_sure = x['Not Sure/Others_1']
@@ -115,26 +97,25 @@ df['Non-PH_1'] = temp_nonph
 df['Not Sure/Others_1'] = temp_notsure
 df['PH_1'] = temp_ph
 
-# df = df.drop('Refused to answer', axis=1)
 df = df.loc[:, df.columns.drop('Refuse to answer')]
+#-------------------------------------------------------------------------------
 
 
+#the goejson file contains error on the iskandar puteri side --> need to fix on the geojson file
 idc = df[df['DUN']=='LAYANG-LAYANG'].index.to_list()
 df.loc[idc[0], 'DUN'] = 'LAYANG - LAYANG'
 
 idc = df[df['DUN']=='KOTA ISKANDAR'].index.to_list()
 df.loc[idc[0], 'DUN'] = 'ISKANDAR PUTERI'
 
-
+#round up the % value
 df['Non-PH_1'] = round(df['Non-PH_1'])
 df['PH_1'] = round(df['PH_1'])
 df['Not Sure/Others_1'] = round(df['Not Sure/Others_1'])
 
 
-# st.write(df[df['DUN']=='KOTA ISKANDAR'])
-
-#layang2 ada beza space with the one in geojson file
-df['predicted_win'] = df[['Non-PH_1','Not Sure/Others_1','PH_1']].idxmax(axis=1)
+#get the party that will win between PH and Non PH
+df['predicted_win'] = df[['Non-PH_1','PH_1']].idxmax(axis=1)
 
 
 
@@ -143,133 +124,25 @@ idc = df.index.to_list()
 for i in idc:
     df.loc[i, 'DUN No'] = dun_dict[df.loc[i,'DUN']]
 
-#will add parliament
-idc = df.index.to_list()
-for i in idc:
-    df.loc[i, 'Parliament'] = dun_par_dict[df.loc[i, 'DUN']]
 
-#will add parliament no
-idc = df.index.to_list()
-for i in idc:
-    df.loc[i, 'Par No'] = par_dict[df.loc[i,'Parliament']]
 
 
 #rearrange the columns
-df = df[['Parliament','Par No','DUN','DUN No','PH_1','Non-PH_1','Not Sure/Others_1','predicted_win']]
+df = df[['DUN','DUN No','PH_1','Non-PH_1','predicted_win']]
 
 #combine for header when hover over each dun
 def combine_dun(x):
-    return "N" + str(x['DUN No']) + " " + str(x['DUN']) + " (" + "P" + str(x['Par No']) + " " + str(x['Parliament']) + ")"
+    return "N" + str(x['DUN No']) + " " + str(x['DUN'])
 
 def combine_percent(x):
     return str(round(x)) + "%"
 
 
 #this contain the % to be included in the map
-df['DUN_Parliament'] = df.apply(combine_dun, axis=1)
+df['DUN_str'] = df.apply(combine_dun, axis=1)
 df['PH'] = df['PH_1'].apply(combine_percent)
 df['Non-PH'] = df['Non-PH_1'].apply(combine_percent)
-df['Not Sure/Others'] = df['Not Sure/Others_1'].apply(combine_percent)
-
-
-#for parliament will need to divide appropriately
-# df
-
-
-
-#sidebar
-# st.sidebar.header("Welcome @user")
-# choice = st.sidebar.selectbox("Choose by Parliamen/DUN",['Parliament', 'DUN'])
-
-# if choice == "DUN":
-#     fig = px.choropleth(data_frame=df,
-#     geojson=my_regions_geo_dun,
-#     locations='DUN', # name of dataframe column
-#     featureidkey='properties.DUN',  # path to field in GeoJSON feature object with which to match the values passed in to locations
-#     color = df['predicted_win'],
-#     hover_name = 'DUN',
-#     custom_data = ['DUN','Non-PH','Not Sure/Others','PH'],
-#     labels = {'predicted_win': 'win'},
-#     # color_discrete_map = {
-#     # 'PH' : '#00BFFF',
-#     # 'Non PH' : '#000080',
-#     # 'Not Sure/Others': '#DC143C',
-#     # },
-#     basemap_visible = False,
-#     scope="asia",
-#     # title = "Map of Johor DUN",
-#     )
-#
-#     fig.update_layout(showlegend=False)
-#     fig.update_geos(fitbounds="locations")
-#
-#     #<extra></extra> tag remove the secondary box
-#     fig.update_traces(
-#     hovertemplate="<br>".join([
-#     "%{customdata[0]}",
-#     "PH: %{customdata[3]}%",
-#     "Non-PH: %{customdata[1]}%",
-#     "Not Sure/Others: %{customdata[2]}% <extra></extra>",
-#     ]))
-#
-#
-#     fig.add_scattergeo(
-#     geojson=my_regions_geo_dun,
-#     locations = df['DUN'],
-#     text = df['DUN No'],
-#     featureidkey="properties.DUN",
-#     mode = 'text',
-#     textfont = dict(size=14, color='white'),
-#     textposition='middle center',
-#     )
-#
-#     fig.update_traces(marker_line_width=0.75, marker_opacity=0.8)
-#
-# #parliament
-# else:
-#     fig = px.choropleth(data_frame=df,
-#     geojson=my_regions_geo_par,
-#     locations='Parliament', # name of dataframe column
-#     featureidkey='properties.Parliament',  # path to field in GeoJSON feature object with which to match the values passed in to locations
-#     color = df['predicted_win'],
-#     hover_name = 'Parliament',
-#     custom_data = ['Parliament','Non-PH','Not Sure/Others','PH'],
-#     labels = {'predicted_win': 'win'},
-#     # color_discrete_map = {
-#     # 'PH' : '#00BFFF',
-#     # 'Non PH' : '#000080',
-#     # 'Not Sure/Others': '#DC143C',
-#     # },
-#     basemap_visible = False,
-#     scope="asia",
-#     )
-#
-#     fig.update_layout(showlegend=False)
-#     fig.update_geos(fitbounds="locations")
-#
-#     fig.update_traces(hovertemplate="<br>".join([
-#     "%{customdata[0]}",
-#     "PH: %{customdata[3]}%",
-#     "Non-PH: %{customdata[1]}%",
-#     "Not Sure/Others: %{customdata[2]}% <extra></extra>",
-#     ]))
-#
-#     fig.add_trace(go.Scattergeo(
-#     geojson=my_regions_geo_par,
-#     locations = df['Parliament'],
-#     text = df['Par No'],
-#     featureidkey="properties.Parliament",
-#     mode = 'text',
-#     textfont = dict(size=14, color='white'),
-#     textposition='top left'
-#     ))
-#
-#     fig.update_traces(marker_line_width=0.75, marker_opacity=0.8)
-#
-#
-# # fig.update_layout(plot_bgcolor='rgb(240, 248, 255)')
-# fig.update_layout(width=1300,height=700)
-# st.plotly_chart(fig, use_container_width=True)
+# df['Not Sure/Others'] = df['Not Sure/Others_1'].apply(combine_percent)
 
 
 #experimenting wiht a better map --> px mapbox
@@ -282,20 +155,28 @@ def create_map():
     opacity=0.5,
     zoom=7,
     center = {"lat": 2.0301, "lon": 103.3185},
-    color = "predicted_win",
+    color = df["predicted_win"],
     color_discrete_map = {
-    'PH' : 'red',
-    'Non PH' : 'blue',
-    'Not Sure/Others': 'grey',
+    'PH_1' : '#D0312D',
+    'Non-PH_1' : '#0000CD',
     },
-    hover_name='DUN_Parliament',
-    hover_data={'DUN No': False, 'predicted_win': False, 'DUN': False,'Parliament': False,'PH': True, 'Non-PH': True, 'Not Sure/Others': True},
+    hover_name='DUN_str',
+    hover_data={'DUN No': False, 'predicted_win': False, 'DUN': False,'PH': True, 'Non-PH': True},
     )
 
     fig.update_geos(fitbounds="locations",visible=False)
+
+    fig.add_scattergeo(
+    geojson=my_regions_geo_dun,
+    locations = df['DUN'],
+    text = df['DUN No'],
+    featureidkey="properties.DUN",
+    mode = 'text')
+
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
     fig.update_layout(showlegend=False)
     st.plotly_chart(fig, use_container_width=True)
+
 
 
 create_map()
@@ -364,7 +245,7 @@ create_map()
 
 
 st.cache(suppress_st_warning=True)
-def card_dun(header, ph_value, non_value, notsure_value, color_lst):
+def card_dun(header, ph_value, non_value, color_lst):
     return f"""
     <div class="mx-auto" style="padding-bottom: 10%">
             <div class="card" style="margin: 3%">
@@ -376,7 +257,7 @@ def card_dun(header, ph_value, non_value, notsure_value, color_lst):
                               <div class="col-7" style="padding: 4%;margin: auto;border-bottom: 1px solid #CCD">
                                       <div style="color: #000000"><strong>PH</strong>
                                           <div class="progress" style="border-radius: 8px;height: 15px">
-                                              <div class="progress-bar" role="progressbar" style="width: {ph_value}%;background-color: #FF0000" aria-valuenow={ph_value} aria-valuemin="0" aria-valuemax="100"></div>
+                                              <div class="progress-bar" role="progressbar" style="width: {ph_value}%;background-color: #D0312D" aria-valuenow={ph_value} aria-valuemin="0" aria-valuemax="100"></div>
                                           </div>
                                       </div>
                               </div>
@@ -387,7 +268,7 @@ def card_dun(header, ph_value, non_value, notsure_value, color_lst):
                               </div>
                           </div>
                           <div class="row" style="padding: auto;margin: auto">
-                              <div class="col-7" style="padding: 4%;margin: auto;border-bottom: 1px solid #CCD">
+                              <div class="col-7" style="padding: 4%;margin: auto;border-bottom: 1px">
                                       <div style="color: #000000"><strong>Non-PH</strong>
                                           <div class="progress" style="border-radius: 8px;height: 15px">
                                               <div class="progress-bar" role="progressbar" style="width: {non_value}%;background-color: #0000CD" aria-valuenow={non_value} aria-valuemin="0" aria-valuemax="100"></div>
@@ -397,20 +278,6 @@ def card_dun(header, ph_value, non_value, notsure_value, color_lst):
                               <div class="col-5" style="padding: 5%;margin:auto; text-align: center;margin-bottom: 1%;">
                                       <div class='progress-label'>
                                       {non_value}%
-                                      </div>
-                              </div>
-                          </div>
-                          <div class="row" style="padding: auto;margin: auto">
-                              <div class="col-7" style="padding: 4%;margin:auto;">
-                                      <div style="color: #000000"><strong>Fence Sitter</strong>
-                                          <div class="progress" style="border-radius: 8px;height: 15px">
-                                              <div class="progress-bar" role="progressbar" style="width: {notsure_value}%;background-color: #A9A9A9" aria-valuenow={notsure_value} aria-valuemin="0" aria-valuemax="100"></div>
-                                          </div>
-                                      </div>
-                              </div>
-                              <div class="col-5" style="padding: 5%;margin:auto; text-align: center;margin-bottom: 1%;">
-                                      <div class='progress-label'>
-                                      {notsure_value}%
                                       </div>
                               </div>
                           </div>
@@ -441,20 +308,26 @@ st.markdown("""
 
 
 def return_color(predict):
-    if predict == 'PH_1':
-        return '#FF0000' #red
-    elif predict == 'Non-PH_1':
-        return '#0000CD' #blue
+    if predict == 'PH':
+        return '#D0312D' #red
     else:
-        return '#A9A9A9' #dark grey
-
+        return '#0000CD' #blue
 
 
 # st.subheader("Prediction for State Seats")
 # st.markdown("<h3 style='height: 2em;padding: 0%;overflow:auto;font-size:25px;font-family: Lato, sans-serif;font-weight:200;padding-top: 0%'>Prediction for State Seats</h3>", unsafe_allow_html=True)
 
+
+def split_str(x):
+    return x['predicted_win'].split("_")[0]
+df['predicted_win'] = df.apply(split_str, axis=1)
+
 #will place a multiselect widget
-options = st.multiselect("State Seats Prediction Wins:", ['PH','Non-PH'], ['PH','Non-PH'])
+#will change here to show PH vs non PH
+options = st.multiselect("State Seats Prediction Win:", ['PH','Non-PH'], ['PH','Non-PH'])
+
+#get the df of ph vs non ph
+df = df[df['predicted_win'].isin(options)]
 
 
 
@@ -462,21 +335,18 @@ options = st.multiselect("State Seats Prediction Wins:", ['PH','Non-PH'], ['PH',
 df.sort_values('DUN No', inplace=True)
 lst_dun = df['DUN'].tolist()
 
-# col1, col2, col3, col4= st.columns(4)
+#to know the remainder left for column order
+func_no = df.shape[0] % 3
 
-for i in range(0, len(lst_dun)-3, 3):
-    header_lst = []
-    ph_lst = []
-    non_lst = []
-    others_lst = []
-    color_lst = []
 
+
+
+for i in range(0, len(lst_dun)-1-func_no, 3):
 
     idc = df[df['DUN']==lst_dun[i]].index.to_list()
     final_str_1 = 'N' + df.loc[idc[0], 'DUN No'] + ' ' + str(df.loc[idc[0], 'DUN'])
     ph_val_1 = df.loc[idc[0], 'PH_1']
     non_val_1 = df.loc[idc[0], 'Non-PH_1']
-    others_val_1 = df.loc[idc[0], 'Not Sure/Others_1']
     color_1 = return_color(df.loc[idc[0], 'predicted_win'])
 
 
@@ -484,14 +354,12 @@ for i in range(0, len(lst_dun)-3, 3):
     final_str_2 = 'N' + df.loc[idc[0], 'DUN No'] + ' ' + str(df.loc[idc[0], 'DUN'])
     ph_val_2 = df.loc[idc[0], 'PH_1']
     non_val_2 = df.loc[idc[0], 'Non-PH_1']
-    others_val_2 = df.loc[idc[0], 'Not Sure/Others_1']
     color_2 = return_color(df.loc[idc[0], 'predicted_win'])
 
     idc = df[df['DUN']==lst_dun[i+2]].index.to_list()
     final_str_3 = 'N' + df.loc[idc[0], 'DUN No'] + ' ' + str(df.loc[idc[0], 'DUN'])
     ph_val_3 = df.loc[idc[0], 'PH_1']
     non_val_3 = df.loc[idc[0], 'Non-PH_1']
-    others_val_3 = df.loc[idc[0], 'Not Sure/Others_1']
     color_3 = return_color(df.loc[idc[0], 'predicted_win'])
     #
     # idc = df[df['DUN']==lst_dun[i+3]].index.to_list()
@@ -508,19 +376,19 @@ for i in range(0, len(lst_dun)-3, 3):
 
     with col1:
         st.markdown(card_dun(
-        final_str_1, round(ph_val_1), round(non_val_1), round(others_val_1), color_1
+        final_str_1, round(ph_val_1), round(non_val_1), color_1
         ), unsafe_allow_html=True)
 
 
     with col2:
         st.markdown(card_dun(
-        final_str_2, round(ph_val_2), round(non_val_2), round(others_val_2), color_2
+        final_str_2, round(ph_val_2), round(non_val_2), color_2
         ), unsafe_allow_html=True)
 
 
     with col3:
         st.markdown(card_dun(
-        final_str_3, round(ph_val_3), round(non_val_3), round(others_val_3), color_3
+        final_str_3, round(ph_val_3), round(non_val_3), color_3
         ), unsafe_allow_html=True)
 
 
@@ -531,40 +399,63 @@ for i in range(0, len(lst_dun)-3, 3):
 
 
 
+if func_no == 2:
+
+    idc = df[df['DUN']==lst_dun[len(lst_dun[-2:])-4]].index.to_list()
+    final_str_1 = 'N' + df.loc[idc[0], 'DUN No'] + ' ' + str(df.loc[idc[0], 'DUN'])
+    ph_val_1 = df.loc[idc[0], 'PH_1']
+    non_val_1 = df.loc[idc[0], 'Non-PH_1']
+    color_1 = return_color(df.loc[idc[0], 'predicted_win'])
 
 
-# for i in range(len(lst_dun[-2:])-4, len(lst_dun[-2:])-2,2):
-idc = df[df['DUN']==lst_dun[len(lst_dun[-2:])-4]].index.to_list()
-final_str_1 = 'N' + df.loc[idc[0], 'DUN No'] + ' ' + str(df.loc[idc[0], 'DUN'])
-ph_val_1 = df.loc[idc[0], 'PH_1']
-non_val_1 = df.loc[idc[0], 'Non-PH_1']
-others_val_1 = df.loc[idc[0], 'Not Sure/Others_1']
-color_1 = return_color(df.loc[idc[0], 'predicted_win'])
+    idc = df[df['DUN']==lst_dun[len(lst_dun[-2:])-3]].index.to_list()
+    final_str_2 = 'N' + df.loc[idc[0], 'DUN No'] + ' ' + str(df.loc[idc[0], 'DUN'])
+    ph_val_2 = df.loc[idc[0], 'PH_1']
+    non_val_2 = df.loc[idc[0], 'Non-PH_1']
+    color_2 = return_color(df.loc[idc[0], 'predicted_win'])
+
+    col1, col2, col3= st.columns(3)
+
+    with col1:
+        st.markdown(card_dun(
+        final_str_1, round(ph_val_1), round(non_val_1), color_1
+        ), unsafe_allow_html=True)
 
 
-idc = df[df['DUN']==lst_dun[len(lst_dun[-2:])-3]].index.to_list()
-final_str_2 = 'N' + df.loc[idc[0], 'DUN No'] + ' ' + str(df.loc[idc[0], 'DUN'])
-ph_val_2 = df.loc[idc[0], 'PH_1']
-non_val_2 = df.loc[idc[0], 'Non-PH_1']
-others_val_2 = df.loc[idc[0], 'Not Sure/Others_1']
-color_2 = return_color(df.loc[idc[0], 'predicted_win'])
-#
-#
-#
-col1, col2, col3= st.columns(3)
-#
-with col1:
-    st.markdown(card_dun(
-    final_str_1, round(ph_val_1), round(non_val_1), round(others_val_1), color_1
-    ), unsafe_allow_html=True)
+    with col2:
+        st.markdown(card_dun(
+        final_str_2, round(ph_val_2), round(non_val_2), color_2
+        ), unsafe_allow_html=True)
+
+    with col3:
+        st.markdown("""
+
+        """, unsafe_allow_html=True)
+
+elif func_no == 1:
+    idc = df[df['DUN']==lst_dun[len(lst_dun[-2:])-3]].index.to_list()
+    final_str_1 = 'N' + df.loc[idc[0], 'DUN No'] + ' ' + str(df.loc[idc[0], 'DUN'])
+    ph_val_1 = df.loc[idc[0], 'PH_1']
+    non_val_1 = df.loc[idc[0], 'Non-PH_1']
+    color_1 = return_color(df.loc[idc[0], 'predicted_win'])
+
+    col1, col2, col3= st.columns(3)
+
+    with col1:
+        st.markdown(card_dun(
+        final_str_1, round(ph_val_1), round(non_val_1), color_1
+        ), unsafe_allow_html=True)
 
 
-with col2:
-    st.markdown(card_dun(
-    final_str_2, round(ph_val_2), round(non_val_2), round(others_val_2), color_2
-    ), unsafe_allow_html=True)
+    with col2:
+        st.markdown("""
 
-with col3:
-    st.markdown("""
+        """, unsafe_allow_html=True)
 
-    """, unsafe_allow_html=True)
+    with col3:
+        st.markdown("""
+
+        """, unsafe_allow_html=True)
+
+else:
+    pass
