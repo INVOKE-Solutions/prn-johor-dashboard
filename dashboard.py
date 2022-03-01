990616287dbe9c421548ae5cdc2320a960dab336
fatinroslan96 <fatin.nabihah1996@gmail.com>
2022-03-01 11:45:04 +0800

    the order of cards and alignment okay

--- dashboard.py
+++ dashboard.py
@@ -16,16 +16,21 @@ import pyautogui
 
 
 
-st.markdown(""" <style>
-#MainMenu {visibility: hidden;}
-footer {visibility: hidden;}
-</style> """, unsafe_allow_html=True)
+# st.markdown(""" <style>
+# #MainMenu {visibility: hidden;}
+# footer {visibility: hidden;}
+# </style> """, unsafe_allow_html=True)
 
 st.markdown("""
 <link href='http://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
 """, unsafe_allow_html=True)
 
-st.markdown("<h2 style='height: 2em;overflow:auto;color: #4682B4;font-size:25px;border-bottom: 1px solid #ccc;font-family: Lato, sans-serif;font-weight:900'>PRN Johor Dashboard</h2>", unsafe_allow_html=True)
+# st.markdown("""
+# <div class="block-container css-12oz5g7 eknhn3m2" style="position: relative;padding-top: 5%">
+# </div>
+# """, unsafe_allow_html=True)
+
+st.markdown("<h2 style='height: 2em;overflow:auto;color: #4682B4;font-size:25px;border-bottom: 1px solid #ccc;font-family: Lato, sans-serif;font-weight:900;padding-top: 0%'>PRN Johor Dashboard</h2>", unsafe_allow_html=True)
 
 
 #dun johor geojson
@@ -268,18 +273,32 @@ df['Not Sure/Others'] = df['Not Sure/Others_1'].apply(combine_percent)
 
 
 #experimenting wiht a better map --> px mapbox
-fig = px.choropleth_mapbox(df, geojson=my_regions_geo_dun,
-locations='DUN',
-featureidkey="properties.DUN",
-mapbox_style="carto-positron",
-opacity=0.5,
-zoom=7,
-center = {"lat": 2.0301, "lon": 103.3185},
-color = 'predicted_win',
-hover_name='DUN_Parliament',
-hover_data={'DUN No': False, 'predicted_win': False, 'DUN': False,'Parliament': False,'PH': True, 'Non-PH': True, 'Not Sure/Others': True},
-)
+st.cache(suppress_st_warning=True)
+def create_map():
+    fig = px.choropleth_mapbox(df, geojson=my_regions_geo_dun,
+    locations='DUN',
+    featureidkey="properties.DUN",
+    mapbox_style="carto-positron",
+    opacity=0.5,
+    zoom=7,
+    center = {"lat": 2.0301, "lon": 103.3185},
+    color = "predicted_win",
+    color_discrete_map = {
+    'PH' : 'red',
+    'Non PH' : 'blue',
+    'Not Sure/Others': 'grey',
+    },
+    hover_name='DUN_Parliament',
+    hover_data={'DUN No': False, 'predicted_win': False, 'DUN': False,'Parliament': False,'PH': True, 'Non-PH': True, 'Not Sure/Others': True},
+    )
 
+    fig.update_geos(fitbounds="locations",visible=False)
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    fig.update_layout(showlegend=False)
+    st.plotly_chart(fig, use_container_width=True)
+
+
+create_map()
 
 
 
@@ -342,24 +361,9 @@ hover_data={'DUN No': False, 'predicted_win': False, 'DUN': False,'Parliament': 
 #         )
 
 
-fig.update_geos(fitbounds="locations",visible=False)
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-fig.update_layout(showlegend=False)
-st.plotly_chart(fig, use_container_width=True)
-
-#
-# #will create a filter option
-# m = st.markdown("""
-# <style>
-# div.Multiselect > multiselect:first-child {
-#     background-color: rgb(204, 49, 49);
-#     width: 30px;
-# }
-# </style>""", unsafe_allow_html=True)
 
 
-#probably need to something to do with width --> control using a fucntion of css responsive
-
+st.cache(suppress_st_warning=True)
 def card_dun(header, ph_value, non_value, notsure_value, color_lst):
     return f"""
     <div class="mx-auto" style="padding-bottom: 10%">
@@ -369,42 +373,42 @@ def card_dun(header, ph_value, non_value, notsure_value, color_lst):
                       </div>
                       <div class='container'>
                           <div class="row" style="padding: auto;margin: auto">
-                              <div class="col-7" style="padding: 4%;">
+                              <div class="col-7" style="padding: 4%;margin: auto;border-bottom: 1px solid #CCD">
                                       <div style="color: #000000"><strong>PH</strong>
-                                          <div class="progress">
+                                          <div class="progress" style="border-radius: 8px;height: 15px">
                                               <div class="progress-bar" role="progressbar" style="width: {ph_value}%;background-color: #FF0000" aria-valuenow={ph_value} aria-valuemin="0" aria-valuemax="100"></div>
                                           </div>
                                       </div>
                               </div>
-                              <div class="col-5" style="padding: 11%; text-align: center">
+                              <div class="col-5" style="padding: 0%;margin:auto; text-align: center;margin-bottom: -1%;">
                                       <p class='progress-label'>
                                       {ph_value}%
                                       </p>
                               </div>
                           </div>
                           <div class="row" style="padding: auto;margin: auto">
-                              <div class="col-7" style="padding: 4%;">
+                              <div class="col-7" style="padding: 4%;margin: auto;border-bottom: 1px solid #CCD">
                                       <div style="color: #000000"><strong>Non-PH</strong>
-                                          <div class="progress">
+                                          <div class="progress" style="border-radius: 8px;height: 15px">
                                               <div class="progress-bar" role="progressbar" style="width: {non_value}%;background-color: #0000CD" aria-valuenow={non_value} aria-valuemin="0" aria-valuemax="100"></div>
                                           </div>
                                       </div>
                               </div>
-                              <div class="col-5" style="padding: 11%; text-align: center">
+                              <div class="col-5" style="padding: 5%;margin:auto; text-align: center;margin-bottom: 1%;">
                                       <div class='progress-label'>
                                       {non_value}%
                                       </div>
                               </div>
                           </div>
                           <div class="row" style="padding: auto;margin: auto">
-                              <div class="col-7" style="padding: 4%;">
+                              <div class="col-7" style="padding: 4%;margin:auto;">
                                       <div style="color: #000000"><strong>Fence Sitter</strong>
-                                          <div class="progress">
+                                          <div class="progress" style="border-radius: 8px;height: 15px">
                                               <div class="progress-bar" role="progressbar" style="width: {notsure_value}%;background-color: #A9A9A9" aria-valuenow={notsure_value} aria-valuemin="0" aria-valuemax="100"></div>
                                           </div>
                                       </div>
                               </div>
-                              <div class="col-5" style="padding: 11%; text-align: center">
+                              <div class="col-5" style="padding: 5%;margin:auto; text-align: center;margin-bottom: 1%;">
                                       <div class='progress-label'>
                                       {notsure_value}%
                                       </div>
@@ -446,22 +450,13 @@ def return_color(predict):
 
 
 
-# st.markdown("""
-# @media only screen and (max-width: 425px) {
-#   /* For desktop: */
-#   .card {width: 100%;margin: auto;margin-right: 5%;margin-left: 5%}
-# }
-#
-# @media only screen and (max-width: 768px) {
-#   /* For desktop: */
-#   .card {width: 60%;margin: auto;margin-right: 5%;margin-left: 5%}
-# }
-# """, unsafe_allow_html=True)
+# st.subheader("Prediction for State Seats")
+# st.markdown("<h3 style='height: 2em;padding: 0%;overflow:auto;font-size:25px;font-family: Lato, sans-serif;font-weight:200;padding-top: 0%'>Prediction for State Seats</h3>", unsafe_allow_html=True)
+
+#will place a multiselect widget
+options = st.multiselect("State Seats Prediction Wins:", ['PH','Non-PH'], ['PH','Non-PH'])
 
 
-
-
-# if (choice == 'DUN'):
 
     #sort by value of DUN no
 df.sort_values('DUN No', inplace=True)
@@ -573,134 +568,3 @@ with col3:
     st.markdown("""
 
     """, unsafe_allow_html=True)
-
-# with col3:
-#     st.markdown("""
-#
-#     """, unsafe_allow_html=True)
-
-#
-# st.markdown("""
-# <br><br>
-# <div class="card" style="width: 30em;margin: auto;margin-right: 20px;margin-bottom: 20px;">
-#     <div class="card-header" style="background-color: #FF0000;color: #FFFFFF">
-#     testing
-#     </div>
-#     <div class="card-body">
-#     <div class="container">
-#         <div class="w3-light-grey w3-round-xlarge" style="width:100px">
-#             <div class="w3-container w3-blue w3-round-large" style="width:0%">0%</div>
-#         </div>
-#     </div>
-#     </div>
-#     <div class="card-footer" style="text-align:left; background-color: #FFF5EE;">
-#         <strong>Demographics <br> </strong>
-#         Malay:  <br>
-#         Chinese: <br>
-#         Indian: <br>
-#         Others: <br>
-#     </div>
-# </div>
-# </div>
-#
-# """, unsafe_allow_html=True)
-
-
-# elif (choice == 'Parliament'):
-#
-#     #sort by value of Par no
-#     df.sort_values('Par No', inplace=True)
-#     # st.write(df)
-#
-#     #sort based on the parliament kod no
-#     par_dict = {k: v for k, v in sorted(par_dict.items(), key=lambda item: item[1])}
-#
-#     lst_par = list(par_dict.keys())
-#
-#
-#     # do in mult of 4 then check if last 2 then 2 je
-#     for i in range(0,len(lst_par)-3, 4):
-#
-#         header_lst = []
-#         ph_lst = []
-#         non_lst = []
-#         others_lst = []
-#         color_lst = []
-#
-#         idc = df[df['Parliament']==lst_par[i]].index.to_list()
-#         final_str_1 = 'P' + df.loc[idc[0], 'Par No'] + ' ' + str(df.loc[idc[0], 'Parliament'])
-#         ph_val_1 = df.loc[idc[0], 'PH_1']
-#         non_val_1 = df.loc[idc[0], 'Non-PH_1']
-#         others_val_1 = df.loc[idc[0], 'Not Sure/Others_1']
-#         color_1 = return_color(df.loc[idc[0], 'predicted_win'])
-#
-#         idc = df[df['Parliament']==lst_par[i+1]].index.to_list()
-#         final_str_2 = 'P' + df.loc[idc[0], 'Par No'] + ' ' + str(df.loc[idc[0], 'Parliament'])
-#         ph_val_2 = df.loc[idc[0], 'PH_1']
-#         non_val_2 = df.loc[idc[0], 'Non-PH_1']
-#         others_val_2 = df.loc[idc[0], 'Not Sure/Others_1']
-#         color_2 = return_color(df.loc[idc[0], 'predicted_win'])
-#
-#         idc = df[df['Parliament']==lst_par[i+2]].index.to_list()
-#         final_str_3 = 'P' + df.loc[idc[0], 'Par No'] + ' ' + str(df.loc[idc[0], 'Parliament'])
-#         ph_val_3 = df.loc[idc[0], 'PH_1']
-#         non_val_3 = df.loc[idc[0], 'Non-PH_1']
-#         others_val_3 = df.loc[idc[0], 'Not Sure/Others_1']
-#         color_3 = return_color(df.loc[idc[0], 'predicted_win'])
-#
-#         idc = df[df['Parliament']==lst_par[i+3]].index.to_list()
-#         final_str_4 = 'P' + df.loc[idc[0], 'Par No'] + ' ' + str(df.loc[idc[0], 'Parliament'])
-#         ph_val_4 = df.loc[idc[0], 'PH_1']
-#         non_val_4 = df.loc[idc[0], 'Non-PH_1']
-#         others_val_4 = df.loc[idc[0], 'Not Sure/Others_1']
-#         color_4 = return_color(df.loc[idc[0], 'predicted_win'])
-#
-#         header_lst.extend([final_str_1, final_str_2, final_str_3, final_str_4])
-#         ph_lst.extend([ph_val_1, ph_val_2, ph_val_3, ph_val_4])
-#         ph_lst = [round(num) for num in ph_lst]
-#         non_lst.extend([non_val_1, non_val_2, non_val_3, non_val_4])
-#         non_lst = [round(num) for num in non_lst]
-#         others_lst.extend([others_val_1, others_val_2, others_val_3, others_val_4])
-#         others_lst = [round(num) for num in others_lst]
-#         color_lst.extend([color_1, color_2, color_3, color_4])
-#
-#
-#         st.markdown(card_dun(
-#         header_lst, ph_lst, non_lst, others_lst, color_lst
-#         ), unsafe_allow_html=True)
-#
-#
-#     for i in range(0, len(lst_par[-2:])-1,2):
-#         header_lst = []
-#         ph_lst = []
-#         non_lst = []
-#         others_lst = []
-#         color_lst = []
-#
-#         idc = df[df['Parliament']==lst_par[i]].index.to_list()
-#         final_str_1 = 'P' + df.loc[idc[0], 'Par No'] + ' ' + str(df.loc[idc[0], 'Parliament'])
-#         ph_val_1 = df.loc[idc[0], 'PH_1']
-#         non_val_1 = df.loc[idc[0], 'Non-PH_1']
-#         others_val_1 = df.loc[idc[0], 'Not Sure/Others_1']
-#         color_1 = return_color(df.loc[idc[0], 'predicted_win'])
-#
-#         idc = df[df['Parliament']==lst_par[i+1]].index.to_list()
-#         final_str_2 = 'P' + df.loc[idc[0], 'Par No'] + ' ' + str(df.loc[idc[0], 'Parliament'])
-#         ph_val_2 = df.loc[idc[0], 'PH_1']
-#         non_val_2 = df.loc[idc[0], 'Non-PH_1']
-#         others_val_2 = df.loc[idc[0], 'Not Sure/Others_1']
-#         color_2 = return_color(df.loc[idc[0], 'predicted_win'])
-#
-#         header_lst.extend([final_str_1, final_str_2])
-#         ph_lst.extend([ph_val_1, ph_val_2])
-#         ph_lst = [round(num) for num in ph_lst]
-#         non_lst.extend([non_val_1, non_val_2])
-#         non_lst = [round(num) for num in non_lst]
-#         others_lst.extend([others_val_1, others_val_2])
-#         others_lst = [round(num) for num in others_lst]
-#         color_lst.extend([color_1, color_2])
-#
-#
-#         st.markdown(card_par_2(
-#         header_lst, ph_lst, non_lst, others_lst, color_lst
-#         ), unsafe_allow_html=True)
